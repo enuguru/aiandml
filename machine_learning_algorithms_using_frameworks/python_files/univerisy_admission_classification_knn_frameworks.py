a1afbc83f127799fa4c2ff374bdc4a768bcd5900
deeplearningcomplete <knowledgetosilence@gmail.com>
2021-09-16 09:55:37 +0530

    new fioles

--- machine_learning_algorithms_using_frameworks/python_files/univerisy_admission_classification_knn_frameworks.py
+++ machine_learning_algorithms_using_frameworks/python_files/univerisy_admission_classification_knn_frameworks.py
@@ -1,3 +1,4 @@
+
 # KNN Classification
 
 # step 1: import the required modules
@@ -13,11 +14,12 @@ names = ['selection', 'gre', 'gpa', 'prestige']
 df = read_csv(filename, names=names)
 
 # step 3: use label encoder to encode the "prestige" categorical value to a number
-# this label encoding is during trainig
+# this label encoding is during trainig - data pre-processing
 # label_encoder object knows how to understand word labels
 label_encoder = preprocessing.LabelEncoder()
 df['prestige']= label_encoder.fit_transform(df['prestige'])
 df['prestige'].unique()
+print("\nThe input train data is \n\n",df,"\n")
 
 # step 4: split in to input and output
 array = df.values
@@ -33,6 +35,8 @@ print("\nThe selected model is ", model)
 filename = '../../datasets/university_admission_classification_small_test.csv'
 names = ['gre', 'gpa', 'prestige']
 newdf = read_csv(filename, names=names)
+print("\nThe input test data is \n\n",newdf,"\n")
+
 
 # step 7: use label encoder to encode the "prestige" categorical value to a number
 # this lable encoding is during testing
@@ -40,11 +44,12 @@ newdf = read_csv(filename, names=names)
 label_encoder = preprocessing.LabelEncoder()
 newdf['prestige']= label_encoder.fit_transform(newdf['prestige'])
 newdf['prestige'].unique()
+print("\nThe input test data is \n\n",newdf,"\n")
 
 # step 7: split in to input and output
 array = newdf.values
 z = array[:,0:3]
-print("\nThe input test data is \n\n",newdf,"\n")
+print("\nThe input test data is \n\n",z,"\n")
 
 # step 8: testing and doing prediction using the model
 res=model.predict(z)
@@ -58,5 +63,3 @@ for val in res:
         print( "selected",end=" ")
     print(end=" ")
 print("\n")
-
-

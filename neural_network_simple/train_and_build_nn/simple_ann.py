3e09e3c7eae70843c266b8c691ee82a1a46e1797
deeplearningcomplete <knowledgetosilence@gmail.com>
2021-10-12 07:31:59 +0530

    new file

---
+++ neural_network_simple/train_and_build_nn/simple_ann.py
@@ -0,0 +1,22 @@
+
+# Introduction to TensorFlow and Keras
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+
+# Commonly used modules
+import numpy as np
+import os
+import sys
+
+# Images, plots, display, and visualization
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import cv2
+import IPython
+from six.moves import urllib
+
+print(tf.__version__)
+

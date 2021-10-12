95825255b64e0a9defb42e257938c7ca934213be
deeplearningcomplete <knowledgetosilence@gmail.com>
2021-10-12 09:47:24 +0530

    new files

--- performance_deep_learning/load_checkpointed_model_googlecolabversion.py
+++ performance_deep_learning/load_checkpointed_model_googlecolabversion.py
@@ -1,21 +1,22 @@
 # How to load and use weights from a checkpoint
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.callbacks import ModelCheckpoint
-import matplotlib.pyplot as plt
 import numpy
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
 # create model
 model = Sequential()
-model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(12, input_dim=8, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(8, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
 # load weights
-model.load_weights("weights.best.hdf5")
+model.load_weights("/content/weights.best.hdf5")
 # Compile model (required to make predictions)
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print("Created model and loaded weights from file")
 # load pima indians dataset
-dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
+dataset = numpy.loadtxt("https://raw.githubusercontent.com/enuguru/aiandml/master/performance_deep_learning/pima-indians-diabetes.csv", delimiter=",")
 # split into input (X) and output (Y) variables
 X = dataset[:,0:8]
 Y = dataset[:,8]

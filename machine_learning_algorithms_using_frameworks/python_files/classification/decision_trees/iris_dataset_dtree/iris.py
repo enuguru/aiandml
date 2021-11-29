962451c1cec96c09eb9753a3f965cc5719192cb3
deeplearningcomplete <knowledgetosilence@gmail.com>
2021-11-29 08:44:15 +0530

    all files

---
+++ machine_learning_algorithms_using_frameworks/python_files/classification/decision_trees/iris_dataset_dtree/iris.py
@@ -0,0 +1,8 @@
+from sklearn.datasets import load_iris
+from sklearn import tree
+iris = load_iris()
+X, y = iris.data, iris.target
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, y)
+tree.plot_tree(clf)
+

4db98a77de3e60675d3bddf2d7e217a92474e823
enuguru <guruforsure@gmail.com>
2023-03-15 16:10:04 +0530

    all files

---
+++ casestudies/mall_customer_segmentation/mall_customer_clustering_analysis_simple.py
@@ -0,0 +1,160 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+# plotly as py
+#import plotly.graph_objs as go
+from sklearn.cluster import KMeans
+import warnings
+import os
+warnings.filterwarnings("ignore")
+#py.offline.init_notebook_mode(connected = True)
+
+# We are reading our data
+filename = 'mall_customers.csv'
+df = pd.read_csv(filename)
+
+df.head()
+
+df.shape
+
+df.describe()
+
+df.dtypes
+
+plt.style.use('fivethirtyeight')
+
+#Histograms
+plt.figure(1 , figsize = (15 , 6))
+n = 0 
+for x in ['Age' , 'Annual Income (k$)' , 'Spending Score (1-100)']:
+    n += 1
+    plt.subplot(1 , 3 , n)
+    plt.subplots_adjust(hspace =0.5 , wspace = 0.5)
+    sns.distplot(df[x] , bins = 20)
+    plt.title('Distplot of {}'.format(x))
+plt.show()
+
+
+#Count Plot of Gender
+plt.figure(1 , figsize = (15 , 5))
+sns.countplot(y = 'Gender' , data = df)
+plt.show()
+
+#Ploting the Relation between Age , Annual Income and Spending Score
+plt.figure(1 , figsize = (15 , 7))
+n = 0 
+for x in ['Age' , 'Annual Income (k$)' , 'Spending Score (1-100)']:
+    for y in ['Age' , 'Annual Income (k$)' , 'Spending Score (1-100)']:
+        n += 1
+        plt.subplot(3 , 3 , n)
+        plt.subplots_adjust(hspace = 0.5 , wspace = 0.5)
+        sns.regplot(x = x , y = y , data = df)
+        plt.ylabel(y.split()[0]+' '+y.split()[1] if len(y.split()) > 1 else y )
+plt.show()
+
+
+plt.figure(1 , figsize = (15 , 6))
+for gender in ['Male' , 'Female']:
+    plt.scatter(x = 'Age' , y = 'Annual Income (k$)' , data = df[df['Gender'] == gender] ,
+                s = 200 , alpha = 0.5 , label = gender)
+plt.xlabel('Age'), plt.ylabel('Annual Income (k$)') 
+plt.title('Age vs Annual Income w.r.t Gender')
+plt.legend()
+plt.show()
+
+plt.figure(1 , figsize = (15 , 6))
+for gender in ['Male' , 'Female']:
+    plt.scatter(x = 'Annual Income (k$)',y = 'Spending Score (1-100)' ,
+                data = df[df['Gender'] == gender] ,s = 200 , alpha = 0.5 , label = gender)
+plt.xlabel('Annual Income (k$)'), plt.ylabel('Spending Score (1-100)') 
+plt.title('Annual Income vs Spending Score w.r.t Gender')
+plt.legend()
+plt.show()
+
+#Distribution of values in Age , Annual Income and Spending Score according to Gender
+plt.figure(1 , figsize = (15 , 7))
+n = 0 
+for cols in ['Age' , 'Annual Income (k$)' , 'Spending Score (1-100)']:
+    n += 1 
+    plt.subplot(1 , 3 , n)
+    plt.subplots_adjust(hspace = 0.5 , wspace = 0.5)
+    sns.violinplot(x = cols , y = 'Gender' , data = df , palette = 'vlag')
+    sns.swarmplot(x = cols , y = 'Gender' , data = df)
+    plt.ylabel('Gender' if n == 1 else '')
+    plt.title('Boxplots & Swarmplots' if n == 2 else '')
+plt.show()
+
+
+# Clustering using K- means
+# 1-Segmentation using Age and Spending Score
+'''Age and spending Score'''
+X1 = df[['Age' , 'Spending Score (1-100)']].iloc[: , :].values
+inertia = []
+for n in range(1 , 11):
+    algorithm = (KMeans(n_clusters = n ,init='k-means++', n_init = 10 ,max_iter=300, 
+                        tol=0.0001,  random_state= 111  , algorithm='elkan') )
+    algorithm.fit(X1)
+    inertia.append(algorithm.inertia_)
+
+algorithm = (KMeans(n_clusters = 4 ,init='k-means++', n_init = 10 ,max_iter=300, 
+                        tol=0.0001,  random_state= 111  , algorithm='elkan') )
+algorithm.fit(X1)
+labels1 = algorithm.labels_
+centroids1 = algorithm.cluster_centers_
+
+h = 0.02
+x_min, x_max = X1[:, 0].min() - 1, X1[:, 0].max() + 1
+y_min, y_max = X1[:, 1].min() - 1, X1[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = algorithm.predict(np.c_[xx.ravel(), yy.ravel()]) 
+
+plt.figure(1 , figsize = (15 , 7) )
+plt.clf()
+Z = Z.reshape(xx.shape)
+plt.imshow(Z , interpolation='nearest', 
+           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+           cmap = plt.cm.Pastel2, aspect = 'auto', origin='lower')
+
+plt.scatter( x = 'Age' ,y = 'Spending Score (1-100)' , data = df , c = labels1 , 
+            s = 200 )
+plt.scatter(x = centroids1[: , 0] , y =  centroids1[: , 1] , s = 300 , c = 'red' , alpha = 0.5)
+plt.ylabel('Spending Score (1-100)') , plt.xlabel('Age')
+plt.show()
+
+# 2-Segmentation using Annual Income and Spending Score
+'''Annual Income and spending Score'''
+X2 = df[['Annual Income (k$)' , 'Spending Score (1-100)']].iloc[: , :].values
+inertia = []
+for n in range(1 , 11):
+    algorithm = (KMeans(n_clusters = n ,init='k-means++', n_init = 10 ,max_iter=300, 
+                        tol=0.0001,  random_state= 111  , algorithm='elkan') )
+    algorithm.fit(X2)
+    inertia.append(algorithm.inertia_)
+
+
+algorithm = (KMeans(n_clusters = 5 ,init='k-means++', n_init = 10 ,max_iter=300, 
+                        tol=0.0001,  random_state= 111  , algorithm='elkan') )
+algorithm.fit(X2)
+labels2 = algorithm.labels_
+centroids2 = algorithm.cluster_centers_
+
+h = 0.02
+x_min, x_max = X2[:, 0].min() - 1, X2[:, 0].max() + 1
+y_min, y_max = X2[:, 1].min() - 1, X2[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z2 = algorithm.predict(np.c_[xx.ravel(), yy.ravel()]) 
+
+plt.figure(1 , figsize = (15 , 7) )
+plt.clf()
+Z2 = Z2.reshape(xx.shape)
+plt.imshow(Z2 , interpolation='nearest', 
+           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+           cmap = plt.cm.Pastel2, aspect = 'auto', origin='lower')
+
+plt.scatter( x = 'Annual Income (k$)' ,y = 'Spending Score (1-100)' , data = df , c = labels2 , 
+            s = 200 )
+plt.scatter(x = centroids2[: , 0] , y =  centroids2[: , 1] , s = 300 , c = 'red' , alpha = 0.5)
+plt.ylabel('Spending Score (1-100)') , plt.xlabel('Annual Income (k$)')
+plt.show()
---
+++ casestudies/salary_prediction_reduced_dataset/salary_regression_linear_regression_frameworks_rmse.py
@@ -0,0 +1,73 @@
+
+# python implementation of simple Linear Regression on salary data of software engineers
+
+# import the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn import model_selection
+
+# step 1: reading the data and splitting it to input and output
+dataset = pd.read_csv('../../datasets/salary_regression_train.csv')
+inputx = dataset.iloc[:, :-1].values
+outputy = dataset.iloc[:, 1].values
+
+
+# step 2: select one thirds of the data for testing and two thirds for training
+input_train, input_test, output_train, output_test = train_test_split(inputx, outputy, test_size = 1/4, random_state = 0)
+
+
+# step 3: selecting the simple Linear Regression model
+model = LinearRegression()
+print("\nThe parameters of the model are\n\n",model.get_params())
+#print(model.set_params())
+print("\nThe model we are using is ", model.fit(input_train, output_train))
+
+
+# step 4: testing or model prediction using testinput
+years = float(input("\nGive number of years of experience  "))
+testinput = [[years]]
+predicted_output = model.predict(testinput)
+print('\nThe number of years of experience is ',testinput) 
+print('\nThe salary for the number of years of experience is ',predicted_output) 
+yes = input("\nCan I proceed\n")
+
+
+# step 5: Visualising the training results
+plt.scatter(input_train, output_train, color = 'red')
+plt.plot(input_train, model.predict(input_train), color = 'yellow')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+# step 6: Printing the testing results
+print("\nThe test input (number of years of experience) is as follows \n")
+print(input_test)
+# model predicting the Test set results
+predicted_output = model.predict(input_test)
+print("\nThe output (salary) for the test input is as follows \n")
+print(predicted_output)
+print(mean_squared_error(output_test, predicted_output,squared=False))
+MSE = np.sqrt(np.square(np.subtract(output_test,predicted_output)).mean())
+print(MSE)
+# we are getting different MSE values becuase the first one calculates MSE while
+# doing testing and the second one calculates MSE while doing training
+kfold = model_selection.KFold(n_splits=10, random_state=7, shuffle=True)
+scoring = 'neg_root_mean_squared_error'
+results = model_selection.cross_val_score(model, input_train, output_train, cv=kfold, scoring=scoring)
+print(results)
+print("RMSE: %.3f (%.3f)" % (results.mean(), results.std()))
+print(np.sqrt(-1*(results.mean())))
+
+
+
+
+
+
+
+
